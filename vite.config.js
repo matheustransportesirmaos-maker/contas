5cdc35b7fc09b5f4389bb1042d01c916da776567
Matheus <matheus.transportesirmaos@gmail.com>
2025-10-02 15:52:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/irmaos/', // para GitHub Pages
+  base: '/contas/', // para GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src') // agora @/ aponta para src/
