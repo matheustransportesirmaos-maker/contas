d74702fe20b913b789184e535d393db0386fa8aa
Matheus <Matheus.transportesirmaos@gmail.com>
2025-10-06 16:59:04 -0300

    Deploy Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // para GitHub Pages
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src') // agora @/ aponta para src/
+      '@': path.resolve(__dirname, './src') // @/ aponta para src/
     }
-  }
-})
+  },
+  build: {
+    chunkSizeWarningLimit: 2000 // aumenta limite para evitar warnings
+  },
+  base: '/' // importante para deploy no Vercel
+});
