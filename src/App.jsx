4fb38b49b4c76d26c89377741b684a1a2c75e12e
Matheus <Matheus.transportesirmaos@gmail.com>
2025-10-06 21:23:04 -0300

    Conectado ao Supabase

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Helmet } from 'react-helmet';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import { Toaster } from './components/ui/toaster';
+import { supabase } from './supabaseClient'
+
 
 function App() {
   const [user, setUser] = useState(null);
